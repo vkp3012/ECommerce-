34599a2ed62e1732652989876aca585678efc0b2
VIVEK KUMAR PATEL <76051796+vkp3012@users.noreply.github.com>
2023-12-04 02:26:57 +0530

    mongodb server is connected

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
-
+const mongoose = require("mongoose")
 const app = express()
 const port = 5000
+const connectDB = async () => {
+    const mongo = mongoose.connect("mongodb://127.0.0.1:27017/e-com");
+    console.log(mongo);
+    const productSchema = new mongoose.Schema({});
+    const product = mongoose.model('product',productSchema);
+    const data = await product.find();
+    console.log(data)
+}
 
-app.get("/",(req,res)=>{
-    res.send("app is working...")
-})
-
+connectDB();
 app.listen(port)
