3daf189087733bd886cbf71675186cb46a630e13
VIVEK KUMAR PATEL <76051796+vkp3012@users.noreply.github.com>
2023-12-04 23:32:37 +0530

    login api is done

--- server/index.js
+++ server/index.js
@@ -7,11 +7,28 @@ const app = express()
 const port = 5000
 app.use(express.json());
 app.use(cors())
+
 app.post("/signup",async (req,res)=>{
     // res.send("api is progress")
     let user = new User(req.body);
     let result = await user.save();
+    result = result.toObject();
+    delete result.password;
     res.send(result)
+})
+
+app.post("/login",async (req,res)=>{
+    if(req.body.email && req.body.password){
+        let user = await User.findOne(req.body).select("-password")
+        if(user){
+            res.send(user)
+        }else{
+            res.send({result : "No User Found"})
+        }
+    }else{
+        res.send({result : "please enter your password"})
+    }
+    
 })
 
 // const connectDB = async () => {
