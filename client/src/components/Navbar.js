91d78826497ac7eb63b934658ba8961528a3dbe0
VIVEK KUMAR PATEL <76051796+vkp3012@users.noreply.github.com>
2023-12-05 14:40:52 +0530

    menu buttton hide than logout and auth name is print

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom"
-
+import Logo from "../images/logo.png"
 const Navbar = () => {
     const auth = localStorage.getItem("user");
     const navigate = useNavigate()
@@ -10,23 +10,38 @@ const Navbar = () => {
     }
     return (
         <div className="nav-link">
-            <ul><Link to="/">🛒Products</Link></ul>
-            <ul><Link to="/add">Add Product</Link></ul>
-            <ul><Link to="/update">Update Product</Link></ul>
-            <ul><Link to="/profile">👧Profile</Link></ul>
+            <img
+                alt="logo1"
+                className="logo"
+                src = {Logo}
+            />
+            { auth ? 
+                <ul>
+                    <li><Link to="/">🛒Products</Link></li>
+                    <li><Link to="/add">Add Product</Link></li>
+                    <li><Link to="/update">Update Product</Link></li>
+                    <li><Link to="/profile">👧Profile</Link></li>
+                    <li><Link onClick={logout} to="/signup">Logout <strong>{JSON.parse(auth).name}</strong></Link></li>
+                </ul>:
+                <ul className="nav-right">
+                    <li><Link to="/signup">SignUp</Link></li>
+                    <li><Link to="/login">Login</Link></li>
+                </ul>  
+            }
+            
             {/* <ul>
                 {   auth ? 
                         <Link onClick={logout} to="/signup">Logout</Link> :  <Link to="/signup">SignUp</Link>
                 }
             </ul>
             <ul><Link to="/login">Login</Link></ul> */}
-                {
+                {/* {
                     auth ? <ul><Link onClick={logout} to="/signup">Logout</Link> </ul>:
                     <>
                         <ul><Link to="/signup">SignUp</Link></ul>
                         <ul><Link to="/login">Login</Link></ul>
                     </>
-                }
+                } */}
         </div>
     )
 }
