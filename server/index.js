78ccbafdf6e5735872b8e465210e9df6a9eb2dfb
VIVEK KUMAR PATEL <76051796+vkp3012@users.noreply.github.com>
2023-12-06 12:55:29 +0530

    jwt verify is done

--- server/index.js
+++ server/index.js
@@ -54,13 +54,13 @@ app.post("/login",async (req,res)=>{
     }    
 })
 
-app.post("/add-product", async(req,res)=>{
+app.post("/add-product", verifyToken, async(req,res)=>{
     let product = new Product(req.body)
     let result = await product.save();
     res.send(result)
 })
 
-app.get("/products",async (req,res)=>{
+app.get("/products",verifyToken, async (req,res)=>{
     let products = await Product.find();
     if(products.length > 0){
         res.send(products)
@@ -69,12 +69,12 @@ app.get("/products",async (req,res)=>{
     }
 })
 
-app.delete("/product/:id",async(req,res)=>{
+app.delete("/product/:id",verifyToken,async(req,res)=>{
     const result = await Product.deleteOne({_id:req.params.id})
     res.send(result)
 })
 
-app.get("/product/:id",async (req,res)=>{
+app.get("/product/:id",verifyToken,async (req,res)=>{
     let result = await Product.findOne({_id:req.params.id});
     if(result){
         res.send(result)
@@ -83,7 +83,7 @@ app.get("/product/:id",async (req,res)=>{
     }
 })
 
-app.put("/product/:id",async (req,res)=>{
+app.put("/product/:id",verifyToken,async (req,res)=>{
     let result = await Product.updateOne(
         { _id : req.params.id},
         {
@@ -93,7 +93,7 @@ app.put("/product/:id",async (req,res)=>{
     res.send(result)
 })
 
-app.get("/search/:key",async (req,res)=>{
+app.get("/search/:key",verifyToken,async (req,res)=>{
     let result = await Product.find({
         "$or" : [
             {name : {$regex : req.params.key}},
@@ -103,6 +103,25 @@ app.get("/search/:key",async (req,res)=>{
     })
     res.send(result)
 })
+
+function verifyToken(req,res,next){
+    let token = req.headers['authorization']
+    if(token){
+        token = token.split(" ")[1];
+        // console.log("middleWare called if",token);
+        jwt.verify(token,jwtKey,(err,valid)=>{
+            if(err){
+                res.status(401).send({ result : "Please provide valid token"})
+            }else{
+                next();
+            }
+        })
+    }else{
+        res.send({ result : "Please add token with headers"})
+    }
+    // console.log("middleware called",token);
+    // next();
+}
 
 // const connectDB = async () => {
 //     const mongo = mongoose.connect("mongodb://127.0.0.1:27017/e-com");
